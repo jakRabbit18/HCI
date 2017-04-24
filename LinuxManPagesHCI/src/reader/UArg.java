f7bdb4386ce282ae1daff27c24df18f213a7fbc3
Jack <jharding2014@yahoo.com>
2017-04-23 20:28:55 -0400

    Added Project

---
+++ LinuxManPagesHCI/src/reader/UArg.java
@@ -0,0 +1,41 @@
+package reader;
+
+public class UArg { 
+	String call, longCall, description;
+	
+	public UArg(String c, String lc, String d) {
+		this.call = c;
+		this.longCall = lc;
+		this.description = d;
+	}
+	
+	public UArg(String c, String d){
+		this(c, null, d);
+	}
+
+	public String getCall() {
+		return call;
+	}
+
+	public void setCall(String call) {
+		this.call = call;
+	}
+
+	public String getLongCall() {
+		return longCall;
+	}
+
+	public void setLongCall(String longCall) {
+		this.longCall = longCall;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+}
